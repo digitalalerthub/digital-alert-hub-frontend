3bb3112a257e5c001aab84a056931128d050675b
Jennifer Gomez <jennifer.gomez@exponenciales.co>
2026-02-27 09:41:59 -0500

    Unificar el enlace de autenticación, implementar enrutamiento basado en roles y limpiar páginas duplicadas

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,22 +1,24 @@
-/* Define estructura del contexto - SOLO types (Vite OK) */
+/* Define estructura del contexto los datos que se compartiran - SOLO types (Vite OK) */
 
-import { createContext } from "react";
+import { createContext } from 'react';
 
 export interface JWTPayload {
-  id: number;
-  email: string;
-  rol: number;
+    id: number;
+    email: string;
+    rol: number;
+    exp: number;
 }
 
 export interface AuthContextType {
-  isLoggedIn: boolean;
-  user: JWTPayload | null;
-  isAdmin: boolean;
-  token: string | null;
-  login: (token: string) => void;
-  logout: () => void;
+    isLoggedIn: boolean;
+    isLoading: boolean;
+    user: JWTPayload | null;
+    isAdmin: boolean;
+    token: string | null;
+    login: (token: string) => void;
+    logout: () => void;
 }
 
 export const AuthContext = createContext<AuthContextType | undefined>(
-  undefined
+    undefined,
 );
